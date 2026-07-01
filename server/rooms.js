81a1e03a6e1aa815e9c6091abdb0375353cc41ca
surajhere777 <surajiiitk1@gmail.com>
2026-07-01 23:53:02 +0530

    Refactor WORDS array and simplify room management
    Updated the WORDS array and modified room timer settings. Removed unused functions related to drawer selection and timer management.

--- server/rooms.js
+++ server/rooms.js
@@ -1,14 +1,17 @@
-﻿const WORDS = ["Apple", "Banana", "Carrot", "Dolphin", "Elephant", "Fire", "Guitar", "House", "Island", "Jungle"];
+const WORDS = ["APPLE", "BANANA", "CHERRY", "DRAGON", "EAGLE", "FLOWER", "GUITAR", "HAMMER", "ISLAND", "JACKET", "KITTEN", "LAPTOP", "MOBILE", "ORANGE", "PENCIL", "ROCKET", "SNAKE", "TURTLE", "UMBRELLA", "VIOLIN"];
 
 const rooms = {};
 
 const createRoom = (roomId) => {
     rooms[roomId] = {
         players: [],
+        currentDrawerIndex: -1,
         currentDrawer: null,
         word: "",
         wordHint: "",
-        timer: 60,
+        timer: 40,
+        round: 0,
+        maxRounds: 10,
         roundActive: false,
         messages: [],
         interval: null
@@ -18,29 +21,4 @@ const createRoom = (roomId) => {
 
 const getRoom = (roomId) => rooms[roomId] || createRoom(roomId);
 
-const selectNewDrawer = (room) => {
-    if (room.players.length === 0) return null;
-    const currentIndex = room.players.findIndex(p => p.id === room.currentDrawer);
-    const nextIndex = (currentIndex + 1) % room.players.length;
-    room.currentDrawer = room.players[nextIndex].id;
-    return room.currentDrawer;
-};
-
-const startTimer = (roomId, io) => {
-    const room = rooms[roomId];
-    if (room.interval) clearInterval(room.interval);
-
-    room.interval = setInterval(() => {
-        if (room.timer > 0 && room.roundActive) {
-            room.timer--;
-            io.to(roomId).emit("room-update", room);
-        } else {
-            clearInterval(room.interval);
-            room.roundActive = false;
-            // Logic to start next round or end game can go here
-            io.to(roomId).emit("room-update", room);
-        }
-    }, 1000);
-};
-
-module.exports = { getRoom, rooms, selectNewDrawer, startTimer, WORDS };
+module.exports = { getRoom, rooms, WORDS };
